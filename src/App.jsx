6bb29f8b3a8c7ad00a32852a9559153aae99c22a
collins kipngeno <kipngenolins01@gmail.com>
2024-05-09 12:53:27 +0300

    commits to Botcard

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.jsx"
+import BotDisplay from "./Components/BotDisplay"
+function App() {
+  return (
+    <div className="App">
+    <BotDisplay/>
+    </div>
+  );
+}
+
+export default App;
