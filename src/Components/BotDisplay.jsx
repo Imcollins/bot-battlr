1e94d8050bc921441800dffcab45620c45b4ca8e
collins kipngeno <kipngenolins01@gmail.com>
2024-05-09 14:37:13 +0300

    Code and comments to BotCard.jsx,BotCollection,BotDisplay

--- src/Components/BotDisplay.jsx
+++ src/Components/BotDisplay.jsx
@@ -3,22 +3,23 @@ import BotsCollection from "./BotCollection";
 import BotArmy from "./YourBotArmy";
 
 function Display() {
+  // State hook to manage the bots data
   let [bots, setBot] = useState([])
-
+  //Effect hook to fetch data from the server when the component 
   useEffect(() => {
 
     fetch("http://localhost:3000/bots")
       .then((resp) => resp.json())
       .then(data => setBot(data))
   }, [])
-
+  // Function to add a bot to the army
   let addBot= (bot)=>{
     setBot(bots.map((b)=>(b.id === bot.id ? {...b, army:true} : b)))
 }
 let removeBot =(bot)=>{
     setBot(bots.map((b)=>(b.id === bot.id ? {...b, army:false} : b)))
 }
-
+//Function to remove a bot from the army
 let deleteBot = (bot)=>{
     let botRemoved =  bots.filter((b)=> b.id !==bot.id);
     setBot((bot)=>botRemoved)
