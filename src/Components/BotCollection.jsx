afefb06d1b52c616a774961d6d1d7dd2906bb014
collins kipngeno <kipngenolins01@gmail.com>
2024-05-09 13:16:32 +0300

    comments on Botcollection

--- src/Components/BotCollection.jsx
+++ src/Components/BotCollection.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import BotCard from "./BotCard";
-
+//This component represents a collection of bot cards
 function BotCollection({ bots, addBot, deleteBot }) {
+  // Mapping over each bot to create BotCard components
   const botItem = bots.map((bot) => {
     return (
       <BotCard
@@ -9,11 +10,11 @@ function BotCollection({ bots, addBot, deleteBot }) {
         bot={bot}
         clickEvent={addBot}
         deleteBot={deleteBot}
-       
       />
     );
   });
   return (
+    // Container for the collection of bot cards
     <div className="container mt-4">
       <div className="row">
       
@@ -22,5 +23,5 @@ function BotCollection({ bots, addBot, deleteBot }) {
     </div>
   );
 }
-
-export default BotCollection;
+//Exporting the BotCollection component
+export default BotCollection; 
