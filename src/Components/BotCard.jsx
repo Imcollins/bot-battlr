7f5fd9bab0c2743d8897dff3f2201f52018233e8
collins kipngeno <kipngenolins01@gmail.com>
2024-05-09 12:54:38 +0300

    App.css styles

--- src/Components/BotCard.jsx
+++ src/Components/BotCard.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-
+//This component represents a card for displaying bot info
 
 function BotCard({ bot, clickEvent, deleteBot }) {
   return (
-
+// The card container with specific styling classes
  <div className="card col-4 mt-5 h-100 shadow p-3 mb-5 bg-body rounded"
  key={bot.id}
         onClick={() => clickEvent(bot)}
  >
-    <div className="card-body"  >
+    <div className="card-body">
  
-   
+         {/* Dispaying the health of the bot*/}
           <h3 className="date" onClick={() => clickEvent(bot)}>Name: {bot.name}
           
           </h3>
-        
+         {/* Dispaying the Description of the bot*/}
           <h3 className="description">Health: {bot.health}</h3>
-       
+          {/* Dispaying the Category of the bot*/}
           <h3 className="category">Damage: {bot.damage}</h3>
-       
+          {/* Dispaying the Armor of the bot*/}
           <h3 className="amount">Armor: {bot.armor}</h3>
        
         {/* <th>
@@ -32,7 +32,7 @@ function BotCard({ bot, clickEvent, deleteBot }) {
             </p>
             {/* Catchphrase: {bot.catchphrase}</h3> */}
       
-      
+          {/* Dispaying the avatar image of the bot*/}
           <img src={bot.avatar_url} alt="url" className="img"> 
 
           </img>
